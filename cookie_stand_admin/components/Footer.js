e699de88f3b64e7378a7024d01ca9f0a90749c89
Ahmad-Alanati <ahmadjehadalanati1@gmail.com>
2023-08-23 13:12:58 +0300

    done

--- cookie_stand_admin/components/Footer.js
+++ cookie_stand_admin/components/Footer.js
@@ -1,7 +1,8 @@
-export default function Footer() {
+export default function Footer(props) {
   return (
     <>
       <footer className="bg-green-500 ipy-6">
+        <p>{props.cookie.length} Location World Wide</p>
         <p>&copy; Wayne Enterprises 2023</p>
       </footer>
     </>
--- cookie_stand_admin/pages/index.js
+++ cookie_stand_admin/pages/index.js
@@ -1,41 +1,28 @@
-import Head from "next/head";
 import { useState } from "react";
-import Header from "@/components/Header"
-import Main from "@/components/Main"
-import Footer from "@/components/Footer";
+import CookieStandAdmin from "@/components/CookieStandAdmin"
 
 export default function Home() {
-  // State to store the cookie stand data
   const [cookie, setCookie] = useState([]);
 
-  // Function to add a new cookie stand
   const submitHandler = (event) => {
     event.preventDefault();
 
-    // Create a new cookie stand object
     const newCookie = {
-      id: Math.random().toString(36).substring(7), // Generate a unique ID
+      id: Math.random().toString(36).substring(7),
       location: event.target.location.value,
       minCustomersPerHour: parseInt(event.target.minCustomersPerHour.value),
       maxCustomersPerHour: parseInt(event.target.maxCustomersPerHour.value),
       avgCookiesPerSale: parseFloat(event.target.avgCookiesPerSale.value),
     };
 
-    // Update the cookie stands state with the new cookie stand
     setCookie([...cookie, newCookie]);
 
-    // Reset the form fields
     event.target.reset();
   };
 
   return (
     <>
-      <Head>
-        <title>Cookie Stand Admin</title>
-      </Head>
-      <Header />
-      <Main handler={submitHandler} cookie={cookie}/>
-      <Footer />
+      <CookieStandAdmin handler={submitHandler} cookie={cookie}/>
     </>
   );
 }
